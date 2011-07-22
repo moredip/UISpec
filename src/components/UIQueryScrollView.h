cd141ef3e2492f65ffac70c826554da4a8ffbbcd
pdkn <keanepaddy@gmail.com>
2011-07-22 18:13:52 +0100

    [~] MOD: UIQueryScrollView
    [~] MOD: all scrolling methods now call scroll:(CGPoint) under the hood
    
    [+] ADD: scrollVertically: , scrollHorizontally:

--- src/components/UIQueryScrollView.h
+++ src/components/UIQueryScrollView.h
@@ -6,19 +6,66 @@
 #import "UIQuery.h"
 
 @interface UIQueryScrollView : UIQuery {
-
+    
 }
 
+/**
+ @brief Scroll to the top of content within the scrollView
+ */
 -(UIQuery*)scrollToTop;
+
+/** 
+ @deprecated In favour of scrollVertically:
+ */
 -(UIQuery *)scrollDown:(int)offset;
+
+/**
+ @brief Scroll to the bottom of content with the scrollView
+ */
 -(UIQuery *)scrollToBottom;
 
+/** 
+ @brief Scroll vertically by offset value
+ 
+ @param offset - amount to scroll by (can be a negative value)
+ */
+-(UIQuery *)scrollVertically:(int)offset;
+
+/** 
+ @brief Scroll horizontally by offset value
+ 
+ @param offset - amount to scroll by (can be a negative value)
+ */
+-(UIQuery *)scrollHorizontally:(int)offset;
+
+/**
+ @brief Scroll left by width of scrollView
+ */
 -(UIQuery *)scrollLeft;
+
+/**
+ @brief Scroll right by width of scrollView
+ */
 -(UIQuery *)scrollRight;
+
+/**
+ @brief Scroll up by height of scrollView
+ */
 -(UIQuery *)scrollUp;
+
+/**
+ @brief Scroll down by height of scrollView
+ */
 -(UIQuery *)scrollDown;
 
-/* UIAutomation API methods unimplemented
+/** 
+ @brief Scroll horizontally by offset value
+ 
+ @param offsetPoint - x & y offset point to scroll by (x or y can be a negative value)
+ */
+-(UIQuery *)scroll:(CGPoint)offsetPoint;
+
+/* TODO unimplemented UIAutomation API methods that could be useful
  -(UIQuery *)scrollToElementWithName;
  -(UIQuery *)scrollToElementWithPredicate;
  -(UIQuery *)scrollToElementWithValueForKey;
